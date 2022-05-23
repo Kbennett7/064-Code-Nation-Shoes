793d345136eea8c56b5c5cfe74e1132910d6ddd7
Kbennett7 <106090393+Kbennett7@users.noreply.github.com>
2022-05-23 09:18:49 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".first").click(function() {
+    $(".mainImage").css("background-color", "gray ");
+                    
+});
+
+$(".second").click(function() {
+    $(".mainImage").css("background-color", "cyan");
+});
+
+$(".third").click(function() {
+    $(".mainImage").css("background-color", "lightgreen");
+   
+});
+
+$(".fourth").click(function() {
+    $(".mainImage").css("background-color", "orange");
+});
